94f8b13c1ed3410c4528a6f5c8e3b2441586813b
Geraud <geraudkameni@gmail.com>
2023-03-05 18:15:08 +0100

    hide the mongoDB URL

--- mqtt_database.py
+++ mqtt_database.py
@@ -5,7 +5,6 @@ db_user = os.environ.get('DB_USER')
 db_password = os.environ.get('DB_PASSWORD')
 
 def insert_data(msg):
-    #client = pymongo.MongoClient("mongodb+srv://geraud:xdA3GhSwcS5fcV9f@mqtt.cyklxjs.mongodb.net/?retryWrites=true&w=majority")
     client = pymongo.MongoClient("mongodb+srv://"+ db_user + ":" + db_password + "@mqtt.cyklxjs.mongodb.net/?retryWrites=true&w=majority")
     db = client.db_mqtt
     collection = db['collection_mqtt']
