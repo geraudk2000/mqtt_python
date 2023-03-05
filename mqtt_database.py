2340090149f316bd5c15a0368908bfb38f4b22b6
Geraud <geraudkameni@gmail.com>
2023-03-05 16:50:10 +0100

    code python mqtt

---
+++ mqtt_database.py
@@ -0,0 +1,13 @@
+import pymongo
+import os
+
+db_user = os.environ.get('DB_USER')
+db_password = os.environ.get('DB_PASSWORD')
+
+def insert_data(msg):
+    #client = pymongo.MongoClient("mongodb+srv://geraud:xdA3GhSwcS5fcV9f@mqtt.cyklxjs.mongodb.net/?retryWrites=true&w=majority")
+    client = pymongo.MongoClient("mongodb+srv://"+ db_user + ":" + db_password + "@mqtt.cyklxjs.mongodb.net/?retryWrites=true&w=majority")
+    db = client.db_mqtt
+    collection = db['collection_mqtt']
+    collection.insert_one(msg).inserted_id
+    client.close()
---
+++ mqtt_publish.py
@@ -0,0 +1,65 @@
+import random
+import time
+from paho.mqtt import client as mqtt_client
+import os
+
+broker = "m6bd9a27-internet-facing-ed79c3f7c10db932.elb.us-east-1.amazonaws.com"
+port = 1883
+topic = "python/mqtt"
+# generate client ID with pub prefix randomly
+client_id = f'python-mqtt-{random.randint(0, 1000)}'
+broker_username = os.environ.get('BROKER_USERNAME')
+broker_password = os.environ.get('BROKER_PASSWORD')
+
+#generate a random payload to send to the broker
+def message_gen(): 
+    message = {
+        "ID" : random.randint(0, 1000000),
+        "NAME": "temperature",
+        "TYPE": "float",
+        "MIN_VALUE": 30,
+        "MAX_VALUE": 40,
+        "MAX_STEP": 0.2
+    }
+    return message
+
+#connect to the broker
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client(client_id)
+    client.username_pw_set(broker_username, broker_password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+
+# Publish message to the broker
+def publish(client):
+
+    while True:
+        data =  message_gen()
+        time.sleep(1)
+        msg = str(data)
+        #msg = f"messages: {data}"
+        print(type(msg))
+        result = client.publish(topic, msg)
+        # result: [0, 1]
+        status = result[0]
+        if status == 0:
+            print(f"Send `{msg}` to topic `{topic}`")
+        else:
+            print(f"Failed to send message to topic {topic}")
+        
+
+def run():
+    client = connect_mqtt()
+    client.loop_start()
+    publish(client)
+
+if __name__ == '__main__':
+    run()
